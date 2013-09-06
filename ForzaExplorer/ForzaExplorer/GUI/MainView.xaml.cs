74bc8466b58a1094ae641706e9a3bcf5b4a1ea07
Giacomo Tagliabue <giacomo.tag@gmail.com>
2013-09-06 16:30:35 -0500

    added resource dictionaries, some icons. main window

---
+++ ForzaExplorer/ForzaExplorer/GUI/MainView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ForzaExplorer.GUI
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
